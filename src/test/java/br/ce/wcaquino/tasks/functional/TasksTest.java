c114fcaf0dc4e4ed6d1c1c075a15d5c0d4f339e8
yagogomes792 <yago.792@hotmail.com>
2020-07-20 09:30:09 -0300

    primeiro commit de testes funcionais

---
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -0,0 +1,118 @@
+package br.ce.wcaquino.tasks.functional;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TasksTest {
+	
+	public WebDriver acessarAplicacao() {
+		// Deve usar o seguinte metodo para selecionar o browser
+		//DesiredCapabilities cap = DesiredCapabilities.chrome();
+		//Quando utilizar o Selenium Grid deve se conectar aos nós com o seguinte metodo
+		// webDriver driver = RemoteWebDriver(new URL(inserir URL do Hub), cap)
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://localhost:8001/tasks");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+	}
+	
+	@Test
+	public void deveSalvarTarefaComSucesso() {
+		WebDriver driver = acessarAplicacao();
+		try {
+			//clicar em Add Todo
+			driver.findElement(By.id("addTodo")).click();
+			
+			//escrever descrição
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+			
+			//escrever a data
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
+			
+			//clicar em salvar
+			driver.findElement(By.id("saveButton")).click();
+			
+			//validar mensagem de sucesso
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+		} finally {
+			// fechar browser
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaSemDescricao() {
+		WebDriver driver = acessarAplicacao();
+		try {
+			//clicar em Add Todo
+			driver.findElement(By.id("addTodo")).click();
+			
+			//escrever a data
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
+			
+			//clicar em salvar
+			driver.findElement(By.id("saveButton")).click();
+			
+			//validar mensagem de sucesso
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Fill the task description", message);
+		} finally {
+			// fechar browser
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaSemData() {
+		WebDriver driver = acessarAplicacao();
+		try {
+			//clicar em Add Todo
+			driver.findElement(By.id("addTodo")).click();
+			
+			//escrever descrição
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+			
+			//clicar em salvar
+			driver.findElement(By.id("saveButton")).click();
+			
+			//validar mensagem de sucesso
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Fill the due date", message);
+		} finally {
+			// fechar browser
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaComDataPassada() {
+		WebDriver driver = acessarAplicacao();
+		try {
+			//clicar em Add Todo
+			driver.findElement(By.id("addTodo")).click();
+			
+			//escrever descrição
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+			
+			//escrever a data
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2010");
+			
+			//clicar em salvar
+			driver.findElement(By.id("saveButton")).click();
+			
+			//validar mensagem de sucesso
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Due date must not be in past", message);
+		} finally {
+			// fechar browser
+			driver.quit();
+		}
+	}
+
+}
